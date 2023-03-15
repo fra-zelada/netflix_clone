1329d682e00b05ef2e5c3d237559299b6519aad5
Francisco <fra.zelada.s@gmail.com>
2023-03-14 22:09:12 -0300

    configuración prisma para usar en lado de cliente

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,7 +1,33 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+// 
+// 
+// const client = global.prismadb || new PrismaClient() ;
+// if ( process.env.NODE_ENV == 'production') global.prismadb = client;
+// 
+// export default client; 
 
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const client = global.prismadb || new PrismaClient() ;
-if ( process.env.NODE_ENV == 'production') global.prismadb = client;
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient;
+    }
+  }
+}
 
-export default client; 
+let prismadb: PrismaClient ;
+
+if (typeof window === "undefined") {
+  if (process.env.NODE_ENV === "production") {
+    prismadb = new PrismaClient();
+  } else {
+    if (!global.prismadb) {
+      global.prismadb = new PrismaClient();
+    }
+
+    prismadb = global.prismadb;
+  }
+}
+
+export default prismadb;
